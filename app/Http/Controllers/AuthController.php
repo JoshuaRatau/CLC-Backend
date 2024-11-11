d96108b1752206d64115854849b73a769b350d8b
Joshua Ratau <joshua@saatplay.com>
2024-11-11 12:12:02 +0200

    first commit with registration api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Str;
+use Exception;
+use Illuminate\Support\Carbon as SupportCarbon;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\View;
+
+
+
+class AuthController extends Controller
+{
+    //
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'surname'=> 'required',
+            'phoneNum' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+        try {
+            $user = User::create([
+                'name' => $request->name,
+                'surname' => $request->surname,
+                'phoneNum' => $request->phoneNum,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+               
+            ]);
+
+            Auth::login($user);
+
+            return response()->json(['user' => $user, 'message' => 'User registered successfully'], 201);
+        } catch (QueryException $e) {
+            return response()->json(['message' => 'Database error'], 500);
+        } catch (ValidationException $e) {
+            return response()->json(['message' => $e->getMessage()], 422);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Registration error'], 500);
+        }
+
+        return response()->json(['message' => 'User registered successfully']);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            //$token = $user->createToken('AuthToken')->accessToken;
+            return response()->json(['user' => $user]);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    }
+
+
+
+
+
+   
+
+   
+
+    
+
+
+   
+
+
+    
+    
+
+}
+
+
+
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+    <!-- Header -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css"
+        integrity="sha512-kIGQ2BYY0fn8U6qZUJhDTH2psV7sL9Xq3I7CHljBOEiZG7fTbhTlZIX7yf1luEHTJbsYX9gENjFpr38xHtBOvw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-NfLLKr2c0xAdTd3a3K5p5ue5FY5zRftXyKj5OawKGKTjjHXitcPZ3xNq3q74p5L5M5ql21xyJ9XzK6/30o6lA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>CLC App</title>
+
+    <!-- bootstrap -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <!------ Include the above in your HEAD tag ---------->
+    <script></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/welcome.css') }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+
+
+
+    <style>
+
+    </style>
+</head>
+
+<body>
+<h1>Backend</h1>
+</body>
